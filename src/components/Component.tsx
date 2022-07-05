4553a6e49fd2a1bf440795f6ecd7bc8f0c64bc5f
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-05 22:12:44 +0530

    Updated the intialized project with material UI and react icons

---
+++ src/components/Component.tsx
@@ -0,0 +1,18 @@
+// Test -------------------------- Importing the Packages ---------------------------------
+import React from 'react'
+
+// Test -------------------------- Importing the styles / other components ----------------
+
+// Test -------------------------- Structure of Props ----------------------------------
+
+// Test -------------------------- The current component ----------------------------------
+const Component = () => {
+  return (
+    <div>Component</div>
+  )
+}
+
+// Test -------------------------- Exporting the current component ------------------------
+export default Component
+
+ 
